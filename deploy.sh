07cc227a4de2161d7e6e68c161e9436fd9c7d84d
kmilewczyk <kmilewczyk96@gmail.com>
2019-09-22 13:56:26 +0200

    Added tmux

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #/bin/env bash
 
-FILE_BINDING_PROFILE=("config_files/profile" "~/.profile")
-FILE_BINDING_NVIM=("config_files/init.vim" "~/.config/nvim/init.vim")
+FILE_BINDING_PROFILE=("config_files/profile" ~/.profile)
+FILE_BINDING_NVIM=("config_files/init.vim" ~/.config/nvim/init.vim)
+FILE_BINDING_TMUX=("config_files/tmux.conf" ~/.tmux.conf)
 
 # Takes src and dst files
 function replace_files {
@@ -14,8 +15,13 @@ function replace_files {
 
 
 function main {
+  pushd $(dirname $0)
+
   replace_files "${FILE_BINDING_PROFILE[@]}"
   replace_files "${FILE_BINDING_NVIM[@]}"
+  replace_files "${FILE_BINDING_TMUX[@]}"
+
+  popd
 }
 
 main "$@"
